7c6df264a061737ea75041445897c5512b5c0706
macBook <Aditya0683624@gmail.com>
2023-06-14 22:21:41 +0530

    Facebook Authentication is on progress.

---
+++ app/src/main/java/com/example/otpverifysample/FacebookAuthenticationActivity.kt
@@ -0,0 +1,11 @@
+package com.example.otpverifysample
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class FacebookAuthenticationActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_facebook_authentication)
+    }
+}
